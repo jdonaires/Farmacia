e4a21c55b5cde19b09ba8d104ae5d83a6298d645
MiriramGonzales2299 <42815398+MiriamGonzales2299users.noreply.github.com>
2018-11-10 13:09:01 -0600

    Diseño de Login
    Ingreso de icono para la pagina principal
    se agrego titulos y requerid para que el usuario tenga la obligacion de llenar los campos

--- PROYECTO/index.php
+++ PROYECTO/index.php
@@ -3,27 +3,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Almacen Farmacia</title>
+	<title>FamiSalud</title>
 	<link rel="stylesheet" type="text/css" href="estilo/estilo.css">
-
+  <link rel="icon" type="img/jpg" href="img/a.jpg">
 </head>
 
 <body class="cajon">
-	<nav class="navegador" align="center">
-		<span>dbFARMACIA</span>
+	<nav class="navegador" align="center"> <!-- Se procedio a agregar el icono para la pagina principal de la web-->
+		<span> Almacenes db FARMISALUD </span>
 	</nav>
 	<form class="formulario">
 		<table >
 			<tr >
-				
-				<td><input type="text" name="" placeholder="Usuario" oninput="check_text(this);"></td>
+
+				<td><input type="text" name="" placeholder="Usuario"  title="se necesita un usuario" required > </td><!--SE AGREGA UN TITULO PARA QUE LA PERSONA QUE SE REGISTRE PUEDA LLENAR EL CAMPO-->
 			</tr>
 			<tr >
-				
-				<td><input type="password" name="" placeholder="Clave" ></td>
+
+				<td><input type="password" name="" placeholder="Clave"  title="se necesita una contraseña" required ></td> <!--SE AGREGA UN TITULO PARA QUE LA PERSONA QUE SE REGISTRE PUEDA LLENAR EL CAMPO-->
 			</tr>
 			<tr>
-				
+
 				<td ><input type="submit" name="" value="LOGIN" style="width:100%;"></td>
 			</tr>
 		</table>
