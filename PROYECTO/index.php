4782252cc5724d8065f17cf2b23ffeaceeb1574f
FlavioCVergara <flavio_cesar_2007@hotmail.com>
2018-11-10 13:21:32 -0600

    Diseño Login
    Se procedio con el diseño del login donde se icono para la pagina principal con su respectivo nombre .

--- PROYECTO/index.php
+++ PROYECTO/index.php
@@ -3,27 +3,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Almacen Farmacia</title>
+	<title>FarmiSalud</title>
 	<link rel="stylesheet" type="text/css" href="estilo/estilo.css">
-
+  <link rel="icon" type="img/jpg" href="img/a.jpg"> <!-- ESTA EDICION SIRVE PARA AGREGAR ICONO AL EMCABEZADO DE LA PAGINA.-->
 </head>
 
 <body class="cajon">
 	<nav class="navegador" align="center">
-		<span>dbFARMACIA</span>
+		<span>FamiSalud</span>
 	</nav>
 	<form class="formulario">
 		<table >
 			<tr >
-				
+
 				<td><input type="text" name="" placeholder="Usuario" oninput="check_text(this);"></td>
 			</tr>
 			<tr >
-				
+
 				<td><input type="password" name="" placeholder="Clave" ></td>
 			</tr>
 			<tr>
-				
+
 				<td ><input type="submit" name="" value="LOGIN" style="width:100%;"></td>
 			</tr>
 		</table>
