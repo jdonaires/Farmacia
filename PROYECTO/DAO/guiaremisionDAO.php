01f12b17088c9296ff2643f421574537d694ed70
yormanCuadros <yorman99_5@hotmail.com>
2018-11-10 16:35:47 -0500

    Campos Guia Remision
    Agregar un campo mas NroGuia

--- PROYECTO/DAO/guiaremisionDAO.php
+++ PROYECTO/DAO/guiaremisionDAO.php
@@ -13,13 +13,14 @@ class GuiaRemisionDAO
 	{
 		try
 		{
-		$statement = $this->pdo->prepare("CALL insertar_guiaremision(?,?,?,?,?,?)");
-    $statement->bindParam(1,$guiaremision->__GET('puntopartida'));
-		$statement->bindParam(2,$guiaremision->__GET('puntollegada'));
-		$statement->bindParam(3,$guiaremision->__GET('fecemision'));
-		$statement->bindParam(4,$guiaremision->__GET('fecinitraslado'));
-		$statement->bindParam(5,$guiaremision->__GET('mottraslado'));
-		$statement->bindParam(6,$guiaremision->__GET('idtransporte'));
+		$statement = $this->pdo->prepare("CALL insertar_guiaremision(?,?,?,?,?,?,?)");
+		$statement->bindParam(1,$guiaremision->__GET('nroguia'));
+    $statement->bindParam(2,$guiaremision->__GET('puntopartida'));
+		$statement->bindParam(3,$guiaremision->__GET('puntollegada'));
+		$statement->bindParam(4,$guiaremision->__GET('fecemision'));
+		$statement->bindParam(5,$guiaremision->__GET('fecinitraslado'));
+		$statement->bindParam(6,$guiaremision->__GET('mottraslado'));
+		$statement->bindParam(7,$guiaremision->__GET('idtransporte'));
 	  $statement -> execute();
 		} catch (Exception $e)
 		{
