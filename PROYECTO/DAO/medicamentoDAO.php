15d828b1dd330b7898e6ca7df16eb8255e2d8f2c
yormanCuadros <yorman99_5@hotmail.com>
2018-11-10 17:15:09 -0500

    Creando Medicamento DAO
    Creacion del php medicamentoDAO

---
+++ PROYECTO/DAO/medicamentoDAO.php
@@ -0,0 +1,27 @@
+<?php
+  require_once('../DAL/DBAccess.php');
+  require_once('../BOL/medicamento.php');
+  class medicamentoDAO
+  {
+      private $pdo;
+      public function __CONSTRUCT()
+      {
+        $dba = new DBAccess();
+        $this->pdo = $dba->get_connection();
+      }
+      public function Registrar(medicamento $medicamento)
+      {
+        try
+        {
+            $statement->bindParam(1,$medicamento->__GET('codmed'));
+            $statement->bindParam(2,$medicamento->__GET('nombre'));
+            $statement->bindParam(3,$medicamento->__GET('nombrelaboratorio'));
+            $statement->bindParam(4,$medicamento->__GET('idlaboratorio'));
+            $statement -> execute();
+        } catch (\Exception $e)
+        {
+            die($e->getMessage());
+        }
+      }
+  }
+?>
