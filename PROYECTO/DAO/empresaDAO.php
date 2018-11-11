09c15a9b464615a2a3d0836acc44d5b917e4696e
CristopherGuerra <minihyus14@gmail.com>
2018-11-11 12:28:36 -0500

    update empresaDAO->ListarEmpresa
    Update empresaDao(ListarEmpresa)

--- PROYECTO/DAO/empresaDAO.php
+++ PROYECTO/DAO/empresaDAO.php
@@ -25,5 +25,31 @@ class empresaDAO
 			die($e->getMessage());
 		}
 	}
+
+	public function listarEmpresa(empresa $empresa)
+	{
+		try
+		{
+			$result = array();
+			$statement = $this->pdo->prepare("call up_listar_empresa(?)");
+			$statement->bindParam(1,$empresa->__GET('RUC'));
+			$statement->execute();
+			foreach($statement->fetchAll(PDO::FETCH_OBJ) as $r)
+			{
+				$emp = new Transporte();
+				$emp->__SET('RUC', $r->RUC);
+				$emp->__SET('RazonSocial', $r->razonsocial);
+				$emp->__SET('Telefono', $r->telefono);
+				$emp->__SET('Direccion', $r->direccion);
+				$emp->__SET('TipoEmpresa', $r->tipoempresa);
+				$result[] = $emp;
+			}
+			return $result;
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
 }
 ?>
