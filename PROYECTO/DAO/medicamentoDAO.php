24c5e454b88087b7b5c3a6454abbea8386e72799
JhairAmoretti <42869403+JhairAmoretti@users.noreply.github.com>
2018-11-12 14:12:00 -0500

    Update PROYECTO/DAO/medicamentoDAO.php

--- PROYECTO/DAO/medicamentoDAO.php
+++ PROYECTO/DAO/medicamentoDAO.php
@@ -14,10 +14,10 @@
         try
         {
             $statement = $this->pdo->prepare("call up_insertar_medicamento(?,?,?,?)");
-            $statement->bindParam(1,$medicamento->__GET('codmed'));
-            $statement->bindParam(2,$medicamento->__GET('nombre'));
-            $statement->bindParam(3,$medicamento->__GET('nombrelaboratorio'));
-            $statement->bindParam(4,$medicamento->__GET('idlaboratorio'));
+            $statement->bindValue(1,$medicamento->__GET('codmed'));
+            $statement->bindValue(2,$medicamento->__GET('nombre'));
+            $statement->bindValue(3,$medicamento->__GET('nombrelaboratorio'));
+            $statement->bindValue(4,$medicamento->__GET('idlaboratorio'));
             $statement -> execute();
         } catch (Exception $e)
         {
