cfe6fb5b09bdae10901c8c4ae553aa4e6d476158
CristopherGuerra <minihyus14@gmail.com>
2018-11-13 04:16:46 -0500

    Update
    Funcion(Actualizar y eliminar ) en Medicamento

--- PROYECTO/DAO/medicamentoDAO.php
+++ PROYECTO/DAO/medicamentoDAO.php
@@ -24,5 +24,36 @@
             die($e->getMessage());
         }
       }
+
+
+      public function actualizarMedicamento(medicamento $medicamento){
+        try
+        {
+          $statement = $this->pdo->prepare("call up_actualizar_medicamento(?,?,?,?)");
+          $statement->bindValue(1,$medicamento->__GET('codmed'));
+          $statement->bindValue(2,$medicamento->__GET('nombre'));
+          $statement->bindValue(3,$medicamento->__GET('nombrelaboratorio'));
+          $statement->bindValue(4,$medicamento->__GET('idlaboratorio'));
+          $statement -> execute();
+        }
+        catch (Exception $e)
+        {
+          die("actualizarMedicamento function  ->".$e->getMessage()."\nLinea-->".$e->getLine());
+        }
+      }
+
+
+      public function eliminarMedicamento($codmed){
+        try
+        {
+          $statement = $this->pdo->prepare("call up_eliminar_medicamento(?)");
+          $statement->bindValue(1,$codmed);
+          $statement -> execute();
+
+        } catch (Exception $e)
+        {
+          die("eliminarMedicamento function  ->".$e->getMessage());
+        }
+      }
   }
 ?>
