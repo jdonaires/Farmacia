36a6d9c476c394baa877bfc2bfbc8268181dc8ea
MatiasSaravia <42815421+Matias1819@users.noreply.github.com>
2018-11-12 19:34:11 -0500

    add menu
    agregando menu

---
+++ PROYECTO/DESIGNER/menu.php
@@ -0,0 +1,104 @@
+<?php 
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ 	<link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+ 	<link rel="stylesheet" type="text/css" href="../estilo/menu.css">
+ </head>
+ <body>
+ 	<div class="title">
+ 		<span > MENU PRINCIPAL</span>
+ 	</div>
+ 	
+ 	<div class="infoUsuario">
+ 		<span>EMPLEADO: PERSONA A</span><br>
+ 		<span>FECHA:<time>2015-07-26T23:44</time> </span>
+ 	</div>
+ 	<table>
+ 		<tr class="titlemenu" >
+ 			<td colspan="3">
+ 				MEDICAMENTO
+ 			</td>
+ 		</tr>
+ 		<tr>
+ 			
+ 			<td>
+ 				<div class="caja">
+ 					<a href="">
+ 						<img src="https://www.flaticon.es/premium-icon/icons/svg/1048/1048568.svg" class="icon">
+ 					</a>AGREGAR MEDICAMENTO
+ 				</div>
+ 			</td>
+ 			<td><div class="caja"><a href=""><img src="https://www.flaticon.es/premium-icon/icons/svg/1048/1048610.svg" class="icon">
+ 			</a>CONSULTAR MEDICAMENTO</div></td>
+ 			<td><div class="caja"><a href=""><img src="https://image.flaticon.com/icons/svg/1201/1201197.svg" class="icon">
+ 			</a>LISTAR MEDICAMENTO</div></td>
+ 		</tr>
+ 		<tr class="titlemenu" >
+ 			<td colspan="3">
+ 				EMPLEADO
+ 			</td>
+ 		</tr>
+ 		<tr>
+ 			
+ 			<td class="col">
+ 				<div class="caja">
+ 					<a href="frmpersona.php">
+ 						<img src="https://www.flaticon.es/premium-icon/icons/svg/1048/1048568.svg" class="icon">
+ 					</a>AGREGAR EMPLEADO
+ 				</div>
+ 			</td>
+ 			<td class="col">
+ 				<div class="caja">
+ 					<a href="">
+ 						<img src="https://www.flaticon.es/premium-icon/icons/svg/1048/1048568.svg" class="icon">
+ 					</a>CONSULTAR EMPLEADO
+ 				</div>
+ 			</td>
+ 			<td class="col">
+ 				<div class="caja">
+ 					<a href="">
+ 						<img src="https://www.flaticon.es/premium-icon/icons/svg/1048/1048568.svg" class="icon">
+ 					</a>LISTAR EMPLEADO
+ 				</div>
+ 			</td>
+ 			
+ 		</tr>
+ 		<tr class="titlemenu" >
+ 			<td colspan="3">
+ 				GUIA DE REMISIÓN
+ 			</td>
+ 		</tr>
+ 		<tr>
+ 			
+ 			<td class="col">
+ 				<div class="caja">
+ 					<a href="">
+ 						<img src="https://www.flaticon.es/premium-icon/icons/svg/1048/1048568.svg" class="icon">
+ 					</a>AGREGAR GUIA DE REMISIÓN
+ 				</div>
+ 			</td>
+ 			<td class="col">
+ 				<div class="caja">
+ 					<a href="">
+ 						<img src="https://www.flaticon.es/premium-icon/icons/svg/1048/1048568.svg" class="icon">
+ 					</a>CONSULTAR GUIA DE REMISIÓN
+ 				</div>
+ 			</td>
+ 			<td class="col">
+ 				<div class="caja">
+ 					<a href="">
+ 						<img src="https://www.flaticon.es/premium-icon/icons/svg/1048/1048568.svg" class="icon">
+ 					</a>LISTAR GUIA DE REMISIÓN
+ 				</div>
+ 			</td>
+ 			
+ 		</tr>
+ 		
+ 		
+ 		
+ 	</table>
+ </body>
+ </html>
