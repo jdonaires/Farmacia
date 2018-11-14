78118e35c120742f5989e931984a5629baaf7989
MiriamGonzales2299 <egc2299@gmail.com>
2018-11-14 08:59:09 -0600

    Correccion de Titulos

--- PROYECTO/DESIGNER/Menu/informealmacen.php
+++ PROYECTO/DESIGNER/Menu/informealmacen.php
@@ -6,7 +6,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Informes de Medicamentos</title>
 </head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
