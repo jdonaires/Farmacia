2af0382dc34ad3d0952c93ed0ff54b3a171def0b
FlavioCVergara <flavio_cesar_2007@hotmail.com>
2018-11-14 11:30:23 -0500

    update index

--- PROYECTO/DESIGNER/index.php
+++ PROYECTO/DESIGNER/index.php
@@ -1,14 +1,14 @@
 <?php
-require_once('BOL/persona.php');
-require_once('DAO/personaDAO.php');
+require_once('../BOL/persona.php');
+require_once('../DAO/personaDAO.php');
 
  ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>FarmiSalud</title>
-	<link rel="stylesheet" type="text/css" href="estilo/estilo.css">
-  <link rel="icon" type="img/jpg" href="img/a.jpg"> <!-- ESTA EDICION SIRVE PARA AGREGAR ICONO AL EMCABEZADO DE LA PAGINA.-->
+	<link rel="stylesheet" type="text/css" href="../estilo/estilo.css">
+  <link rel="icon" type="img/jpg" href="../img/a.jpg"> <!-- ESTA EDICION SIRVE PARA AGREGAR ICONO AL EMCABEZADO DE LA PAGINA.-->
 </head>
 
 <body  class="cajon">
@@ -18,7 +18,7 @@ require_once('DAO/personaDAO.php');
 	<form action="buscarpersona.php" method="post" class="formulario" >
 		<table >
 			<center>
-			<img src="img/login.png" >  <!--posicion de Imagen login-->
+			<img src="../img/login.png" >  <!--posicion de Imagen login-->
 		   </center>
 			<tr >
 
@@ -71,11 +71,7 @@ require_once('DAO/personaDAO.php');
 									</tr>
 					<?php endforeach;
 			}
-			else
-			{
-					/*echo 'El Dni no se encuentra en la base de datos!';*/
-					echo "<script> alert('El Dni no se encuentra en la base de datos!'); </script>";
-			}
+			
 			?>
 			</table>
 			<?php
