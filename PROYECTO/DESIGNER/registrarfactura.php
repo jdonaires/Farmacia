555b2f37ba88777de8267f46b8be83dd49d9d1f9
Aldair Bendezu Yarasca <42815626+bendezuuyarasca9991@users.noreply.github.com>
2018-11-14 10:23:48 -0500

    Colocacion de condicionales
    Condicionales para que no se escriban palabras con tildes y la letra ñ

--- PROYECTO/DESIGNER/registrarfactura.php
+++ PROYECTO/DESIGNER/registrarfactura.php
@@ -16,6 +16,32 @@
 
 			header('Location: registrarfactura.php');
 	}
+
+// Estas condicionales son para las palabras con tildes y la letra ñ //
+
+	// Modificamos las variables pasadas por URL
+foreach( $_GET as $variable => $valor ){
+$_GET [ $variable ] = str_replace ( "'" , "" , $_GET [ $variable ]);
+$_GET [ $variable ] = str_replace ( "\"" , "" , $_GET [ $variable ]);
+}
+// Modificamos las variables de formularios
+foreach( $_POST as $variable => $valor ){
+$_POST [ $variable ] = str_replace ( "'" , "" , $_POST [ $variable ]);
+$_POST [ $variable ] = str_replace ( "\"" , "" , $_POST [ $variable ]);
+$_POST [ $variable ] = str_replace ( "á" , "&aacute;" , $_POST [ $variable ]);
+$_POST [ $variable ] = str_replace ( "é" , "&eacute;" , $_POST [ $variable ]);
+$_POST [ $variable ] = str_replace ( "í" , "&iacute;" , $_POST [ $variable ]);
+$_POST [ $variable ] = str_replace ( "ó" , "&oacute;" , $_POST [ $variable ]);
+$_POST [ $variable ] = str_replace ( "ú" , "&uacute;" , $_POST [ $variable ]);
+$_POST [ $variable ] = str_replace ( "Á" , "&Aacute;" , $_POST [ $variable ]);
+$_POST [ $variable ] = str_replace ( "É" , "&Eacute;" , $_POST [ $variable ]);
+$_POST [ $variable ] = str_replace ( "Í" , "&Iacute;" , $_POST [ $variable ]);
+$_POST [ $variable ] = str_replace ( "Ó" , "&Oacute;" , $_POST [ $variable ]);
+$_POST [ $variable ] = str_replace ( "Ú" , "&Uacute;" , $_POST [ $variable ]);
+$_POST [ $variable ] = str_replace ( "ñ" , "&ntilde;" , $_POST [ $variable ]);
+$_POST [ $variable ] = str_replace ( "Ñ" , "&Ntilde;" , $_POST [ $variable ]);
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
