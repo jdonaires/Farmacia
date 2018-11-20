01942fa7921c6b242b6cc1c14215ac06ebff909e
YATACOBLAS <juan_blas_1905@hotmail.com>
2018-11-20 00:13:30 -0500

    mod regempleado

--- PROYECTO/DESIGNER/registrarEmpleado.php
+++ PROYECTO/DESIGNER/registrarEmpleado.php
@@ -1,17 +1,33 @@
 <?php
 require_once('../BOL/empleado.php');
 require_once('../DAO/EmpleadoDAO.php');
+require_once('../BOL/persona.php');
+require_once('../DAO/personaDAO.php');
 
 $emp=new Empleado();
 $empDAO=new EmpleadoDAO();
+$per = new Persona();
+$perDAO = new PersonaDAO();
+
 
 if (isset($_POST['guardar'])) {
+  $per->__SET('dni', $_POST['dni']);
+  $per->__SET('nombres',$_POST['nombre']);
+  $per->__SET('apellidos',$_POST['apellido']);
+  $per->__SET('correo', $_POST['correo']);
+  $per->__SET('genero', $_POST['genero']);
+  $per->__SET('fecnac', $_POST['fecnac']);
+  $per->__SET('direccion', $_POST['direccion']);
+  $per->__SET('tipopersona', $_POST['Empleado']);
+
+
     $emp->__SET('dni', $_POST['dni']);
     $emp->__SET('turno',$_POST['turno']);
     $emp->__SET('especialidad',$_POST['especialidad']);
     $emp->__SET('clave', $_POST['clave']);
     $emp->__SET('usuario', $_POST['usuario']);
-    $empDAO->listarEmpleado($per);
+    $perDAO->insertarPersona($per);
+    $empDAO->listarEmpleado($emp);
 
     header('Location: registrarEmpleado.php');
 }
@@ -29,19 +45,30 @@ if (isset($_POST['guardar'])) {
   		<div class="form__top">
   			<h1>Formulario <span>Registro de Empleados</span></h1>
   		</div>
-  		<form class="form__reg" action="">
-              <input class="input-50" type="text" placeholder="DNI" required autofocus><br>
+  		<form class="form__reg" action="registrarpersona.php" method="post" >
+              <input class="input-50" type="text" placeholder="DNI"  name="dni "required autofocus><br>
+              <input class="input-50" type="Nombre" placeholder="Nombres" name="nombre" required autofocus><br>
+              <input class="input-50" type="Apellido" placeholder="Apellidos"   name="Apellido" required autofocus><br>
+              <input class="input-50" type="Correo" placeholder="Correo"    name="correo" required autofocus><br>
+              <input class="input-50" type="date"  name="fecnac" required autofocus><br>
 
-              <select class="input-100" class="" name="">
+              <select class="input-100" class="" name="genero">
+                <option value="M">Masculino</option>
+                <option value="F">Femenino</option>
+
+              </select><br><br>
+              <input class="input-50" type="Direccion" placeholder="Correo"  name="direccion"  required autofocus><br>
+
+              <select class="input-100"  name="turno">
                 <option value="Mañana">Mañana</option>
                 <option value="Tarde">Tarde</option>
                 <option value="Noche">Noche</option>
               </select><br><br>
-              <input class="input-100" type="tex" placeholder="Especialidad" required><br>
-              <input class="input-50" type="password" placeholder="Clave" required><br>
+              <input class="input-100" type="tex" placeholder="especialidad" required><br>
               <input class="input-50" type="text" placeholder="usuario" required><br>
+              <input class="input-50" type="password" placeholder="clave" required><br>
               <div class="btn__form">
-              	<input class="btn__submit" type="submit" value="REGISTRAR"><br>
+              	<input class="btn__submit" type="submit" value="REGISTRAR" name=guardar""><br>
               	<input class="btn__reset" type="reset" value="LIMPIAR">
               </div>
   		</form>
