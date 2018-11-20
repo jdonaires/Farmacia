cf26025d6176103312d5612f00e02ec521d134c3
JhairAmoretti <42869403+JhairAmoretti@users.noreply.github.com>
2018-11-20 13:05:25 -0500

    Update PROYECTO/DESIGNER/FComponentes.php

--- PROYECTO/DESIGNER/FComponentes.php
+++ PROYECTO/DESIGNER/FComponentes.php
@@ -19,36 +19,119 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0">
-	<title>Componentes de Medicamentos</title>
-
-	<title>Formulario de Componentes</title>
-
-	<link rel="stylesheet" type="text/css" href="../estilo/fcomponentes.css">
-	<link rel="icon" type="img/jpg" href="../img/a.jpg">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Componentes </title>
 </head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<style>
+header{
+  background: #202E37;
+    color: white;
+}
+nav{
+  background:#202E37;
+  color: white;
+
+}
+.cuerpo{
+  margin-top:110px;
+}
+</style>
 <body>
-  <div>
-	</br>
-  	<h1>COMPONENTES DE MEDICAMENTOS</h1>
-    <form class="contact_form">
-		</br>
-    	<input type="text" style="width : 100 % ;"  placeholder="Codigo - Detalle Medidamento" required>
+  <header>
 
-      <input type="text" style="width : 100 % ;"  placeholder="Nombre del Medicamento" required>
+    <nav class="navbar navbar-fixed-top ">
+<div class="container">
+  <h3><span class="glyphicon glyphicon-fire"> </span > Registrar Componentes</h3>
 
-			<input type="text" style="width : 100 % ;"  placeholder="Tipo" required >
+<ul class="nav nav-pills">
+  <li role="presentation"><a href="menu/menu.html">Inicio</a></li>
+</ul>
+</div>
+    </nav>
 
-			<input type="text" style="width : 100 % ;"  placeholder="Cantidad"required >
+  </header>
+  <div class=" container cuerpo">
+ <section class="main">
+   <BR>
+<div class="panel panel-primary">
+<div class="panel-heading"><h3>Componentes</h3></div>
+<div class="panel-body">
+<form class="form-horizontal" action="FComponentes.php" method="post">
+  <div class="row container">
+   <div class="col-md-6">
+     <div class="form-group has-success	">
+      <label class="control-label col-md-2">Componentes:</label>
+      <div class="col-md-7">
+       <input  class =" form-control" type="text" name="componentes" placeholder="Componentes" required >
+     </div>
+     </div>
+    <div class="form-group has-success">
+     <label class="control-label col-md-2" >Precio Actual</label>
+     <div class="col-md-7">
+     <input type="text" name="Precio Actual" class="form-control" placeholder="Precio Actual" required>
+     </div>
+   </div>
+   <div class="form-group has-success">
+    <label  class="control-label col-md-2" for="">Almacen</label>
+    <div class="col-md-7">
+      <input class="form-control" type="text" name="Almacen" placeholder="Almacen" required>
+     </div>
+   </div>
+<div class="form-group has-success">
+  <label  class="control-label col-md-2" for="">Tipo Presentacion</label>
+    <div class="col-md-7">
+    <input class="form-control" type="text" name="Tipo Presentacion" placeholder="Tipo Presentacion" required>
+    </div>
+</div>
+   </div>
+   <div class="col-md-6">
 
-			<input type="text" style="width : 100 % ;"  placeholder="Unidad de Medida" required>
+         <div class="form-group has-success">
+          <label  class="control-label col-md-2" >Posologia</label>
+          <div class="col-md-7">
+            <input class="form-control" type="text" name="Posologia" placeholder="Posologia" required>
+           </div>
+         </div>
+         <div class="form-group has-success">
+          <label  class="control-label col-md-2" >Nombre</label>
+          <div class="col-md-7">
+            <input class="form-control" type="text" name="nombre" placeholder="Nombre" required>
+           </div>
+         </div>
+         <div class="form-group has-success">
+          <label  class="control-label col-md-2" >Tipo: </label>
+          <div class="col-md-7">
+            <input class="form-control" type="text" name="Tipo" placeholder="Tipo" required>
+           </div>
+         </div>
+         <div class="form-group has-success">
+          <label  class="control-label col-md-2" >Cantidad</label>
+          <div class="col-md-7">
+            <input class="form-control" type="text" name="cantidad" placeholder="Cantidad" required>
+           </div>
+         </div>
 
-			<input type="submit" id="button" value="Guardar" />
-
-    </form>
+   </div>
   </div>
+        <div class="form-group">
+         <div  class="col-md-7 col-md-offset-2">
+           <button class="btn btn-success btn-block" name="guardar" value="guardar">Guardar</button>
+         </div>
+        </div>
+
+</form>
+
+</div>
+</section>
+</div>
+
+
 </body>
 </html>
