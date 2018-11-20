599796d6acaec5713fc016697081c6c7a420596f
YATACOBLAS <juan_blas_1905@hotmail.com>
2018-11-20 01:08:19 -0500

    mod empleado

--- PROYECTO/DESIGNER/registrarEmpleado.php
+++ PROYECTO/DESIGNER/registrarEmpleado.php
@@ -1,19 +1,19 @@
 <?php
 require_once('../BOL/empleado.php');
-require_once('../DAO/EmpleadoDAO.php');
+require_once('../DAO/empleadoDAO.php');
 require_once('../BOL/persona.php');
 require_once('../DAO/personaDAO.php');
 
-$emp=new Empleado();
-$empDAO=new EmpleadoDAO();
-$per = new Persona();
-$perDAO = new PersonaDAO();
+$emp=new empleado();
+$empDAO=new empleadoDAO();
+$per = new persona();
+$perDAO = new personaDAO();
 
 
 if (isset($_POST['guardar'])) {
   $per->__SET('dni', $_POST['dni']);
-  $per->__SET('nombres',$_POST['nombre']);
-  $per->__SET('apellidos',$_POST['apellido']);
+  $per->__SET('nombre',$_POST['nombre']);
+  $per->__SET('apellido',$_POST['apellido']);
   $per->__SET('correo', $_POST['correo']);
   $per->__SET('genero', $_POST['genero']);
   $per->__SET('fecnac', $_POST['fecnac']);
@@ -26,8 +26,8 @@ if (isset($_POST['guardar'])) {
     $emp->__SET('especialidad',$_POST['especialidad']);
     $emp->__SET('clave', $_POST['clave']);
     $emp->__SET('usuario', $_POST['usuario']);
-    $perDAO->insertarPersona($per);
-    $empDAO->listarEmpleado($emp);
+
+    $empDAO->insertarEmpleado($emp,$per);
 
     header('Location: registrarEmpleado.php');
 }
@@ -45,10 +45,10 @@ if (isset($_POST['guardar'])) {
   		<div class="form__top">
   			<h1>Formulario <span>Registro de Empleados</span></h1>
   		</div>
-  		<form class="form__reg" action="registrarpersona.php" method="post" >
+  		<form class="form__reg" action="registrarEmpleado.php" method="post" >
               <input class="input-50" type="text" placeholder="DNI"  name="dni "required autofocus><br>
               <input class="input-50" type="Nombre" placeholder="Nombres" name="nombre" required autofocus><br>
-              <input class="input-50" type="Apellido" placeholder="Apellidos"   name="Apellido" required autofocus><br>
+              <input class="input-50" type="Apellido" placeholder="Apellidos"   name="apellido" required autofocus><br>
               <input class="input-50" type="Correo" placeholder="Correo"    name="correo" required autofocus><br>
               <input class="input-50" type="date"  name="fecnac" required autofocus><br>
 
@@ -57,7 +57,7 @@ if (isset($_POST['guardar'])) {
                 <option value="F">Femenino</option>
 
               </select><br><br>
-              <input class="input-50" type="Direccion" placeholder="Correo"  name="direccion"  required autofocus><br>
+              <input class="input-50" type="Direccion" placeholder="Direccion"  name="direccion"  required autofocus><br>
 
               <select class="input-100"  name="turno">
                 <option value="Mañana">Mañana</option>
@@ -68,7 +68,7 @@ if (isset($_POST['guardar'])) {
               <input class="input-50" type="text" placeholder="usuario" required><br>
               <input class="input-50" type="password" placeholder="clave" required><br>
               <div class="btn__form">
-              	<input class="btn__submit" type="submit" value="REGISTRAR" name=guardar""><br>
+              	<input class="btn__submit" type="submit" value="Guardar" name=guardar""><br>
               	<input class="btn__reset" type="reset" value="LIMPIAR">
               </div>
   		</form>
