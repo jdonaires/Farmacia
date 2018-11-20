899815c345cf3a2f594cf78057fc71c319623619
JhairAmoretti <42869403+JhairAmoretti@users.noreply.github.com>
2018-11-20 13:27:56 -0500

    Update PROYECTO/DESIGNER/FComponentes.php

--- PROYECTO/DESIGNER/FComponentes.php
+++ PROYECTO/DESIGNER/FComponentes.php
@@ -87,8 +87,23 @@ nav{
     <input class="form-control" type="text" name="cantidad" placeholder="Cantidad" required>
     </div>
 </div>
-
-
+<div class="col-md-6">
+			<div class="form-group has-success">
+			 <label  class="control-label col-md-2" >Posologia</label>
+			</div>
+			<div class="form-group has-success">
+			 <label  class="control-label col-md-2" >Dosis</label>
+			 <div class="col-md-7">
+				 <input class="form-control" type="text" name="dosis" placeholder="Dosis" required>
+				</div>
+			</div>
+			<div class="form-group has-success">
+			 <label  class="control-label col-md-2" >Indicaciones:</label>
+			 <div class="col-md-7">
+				 <input class="form-control" type="text" name="indicaciones" placeholder="Indicaciones" required>
+				</div>
+   </div>
+        
 </form>
 
 </div>
