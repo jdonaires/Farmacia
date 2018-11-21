ef96184c9a52e0d5d415c425ec648da98080ca60
Cristopher Guerra <minihyus14@gmail.com>
2018-11-21 12:56:14 -0500

    Correccion
    DIreccion BD

--- PROYECTO/DAL/DBAccess.php
+++ PROYECTO/DAL/DBAccess.php
@@ -9,7 +9,7 @@
     public function __CONSTRUCT()
     {
       try {
-          $this->conn = new PDO('mysql:host=localhost;dbname=farmacia', 'root', 'abc123');
+        $this->conn = new PDO("mysql:host=software-if.mysql.database.azure.com;dbname=farmacia", "bd_usuario@software-if", "123456");
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch (PDOException $e ) {
         echo "DBAcess error: " .$e->getMessage();
