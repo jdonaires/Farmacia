00e378f014aa868fbe87be85645102b61a75f092
esmeralda123francesca <esmeralda250297@gmail.com>
2018-11-20 22:15:00 -0500

    Update informealmacen.php

--- PROYECTO/DESIGNER/informealmacen.php
+++ PROYECTO/DESIGNER/informealmacen.php
@@ -31,7 +31,9 @@ nav{
     <nav class="navbar navbar-fixed-top ">
 <div class="container">
   <h2><span class="glyphicon glyphicon-fire"> </span > Informe Almacen</h2>
-
+  <ul class="nav nav-pills">
+  <li role="presentation"><a href="menu.html">Inicio</a></li>
+</ul>
 
 </div>
     </nav>
