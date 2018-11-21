a8891ebe01d9b79be8a6170db06e0d2979b5fd28
Richard <guerrahuarcaya@gmail.com>
2018-11-21 12:03:28 -0500

    Update medicamento.php

--- PROYECTO/BOL/medicamento.php
+++ PROYECTO/BOL/medicamento.php
@@ -8,12 +8,12 @@ class medicamento
 
 
 	public function __GET($x)
-	{ 
-		return $this->$x; 
+	{
+		return $this->$x;
 	}
 	public function __SET($x, $y)
-	{ 
-		return $this->$x = $y; 
+	{
+		return $this->$x = $y;
 	}
 }
 ?>
