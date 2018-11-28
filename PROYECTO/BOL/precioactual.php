3e7dad62fffe8ed68c5825773de5a0a743c43618
richardguerrahuarcaya <guerrahuarcaya@gmail.com>
2018-11-27 19:54:52 -0500

    Update precioactual.php

--- PROYECTO/BOL/precioactual.php
+++ PROYECTO/BOL/precioactual.php
@@ -7,12 +7,12 @@ class precioactual
 	private $fecactualizacion;
 
 	public function __GET($x)
-	{ 
-		return $this->$x; 
+	{
+		return $this->$x;
 	}
 	public function __SET($x, $y)
-	{ 
-		return $this->$x = $y; 
+	{
+		return $this->$x = $y;
 	}
 }
 ?>
